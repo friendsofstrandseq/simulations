629767171781fdb8b4576a562836486462f28a13
Sascha Meiers <meiers@embl.de>
2018-03-27 10:55:01 +0200

    New plot script to plot both simulated as well as predicted SVs

--- utils/plot_sv_calls.R
+++ utils/plot_sv_calls.R
@@ -3,6 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE.md or http://www.opensource.org/licenses/mit-license.php.
 
+library(dplyr)
 library(data.table)
 library(ggplot2)
 library(scales)
@@ -14,15 +15,51 @@ format_Mb   <- function(x) {paste(comma(x/1e6), "Mb")}
 cells_per_page <- 8
 
 
-f_counts   = "counts/simulation5-50000/50000_fixed.txt.gz"
-f_segments = "segmentation2/..."
-f_sv_calls = "sv_calls/..."
-f_out      = "sv_calls/..."
+# f_counts   = "counts/simulation5-50000/50000_fixed.txt.gz"
+# f_segments = "segmentation2/simulation5-50000/50000_fixed.few.txt"
+# f_sv_calls = "sv_calls/simulation5-50000/50000_fixed.few/maryam.txt"
+# f_sv_simul = "simulation/variants/genome5-50000.txt"
+# f_out      = "sv_calls/simulation5-50000/50000_fixed.few/maryam"
 
 f_counts   = snakemake@input[["counts"]]
 f_segments = snakemake@input[["segs"]]
 f_sv_calls = snakemake@input[["svs"]]
+f_sv_simul = snakemake@input[["simul"]]
 f_out      = snakemake@output[[1]]
+f_out      = sub('\\.[chr0-9_]+\\.pdf$','', f_out)
+
+
+
+
+### Colors for background
+manual_colors = c(  # duplications
+                  simul_hom_dup   = "firebrick4",
+                  dup_hom         = muted("firebrick4", 70, 50),
+                  simul_het_dup   = "firebrick2",
+                  dup_h1          = muted("firebrick2", 90, 30),
+                  dup_h2          = muted("firebrick2", 90, 30),
+                    # deletions
+                  simul_hom_del   = "dodgerblue4",
+                  del_hom         = muted("dodgerblue4", 50, 60),
+                  simul_het_del   = "dodgerblue2",
+                  del_h1          = muted("dodgerblue2", 80, 50),
+                  del_h2          = muted("dodgerblue2", 80, 50),
+                    # inversions
+                  simul_hom_inv   = "chartreuse4",
+                  inv_hom         = muted("chartreuse4", 80, 50),
+                  simul_het_inv   = "chartreuse2",
+                  inv_h1          = muted("chartreuse2", 100, 60),
+                  inv_h2          = muted("chartreuse2", 100, 60),
+                    # other SVs
+                  simul_false_del = "darkgrey",
+                  simul_inv_dup   = "darkgoldenrod2",
+                  idup_h1         = muted("darkgoldenrod2", 80, 70),
+                  idup_h2         = muted("darkgoldenrod2", 80, 70),
+                    # background
+                  bg1 = "#ffffff",
+                  bg2 = "#cccccc")
+
+
 
 
 
@@ -34,6 +71,7 @@ if (grepl('\\.gz$',f_counts)) {
 } else {
     counts = fread(f_counts)
 }
+
 assert_that("chrom"  %in% colnames(counts),
             "start"  %in% colnames(counts),
             "end"    %in% colnames(counts),
@@ -41,140 +79,51 @@ assert_that("chrom"  %in% colnames(counts),
             "sample" %in% colnames(counts),
             "cell"   %in% colnames(counts),
             "w"      %in% colnames(counts),
-            "c"      %in% colnames(counts))
+            "c"      %in% colnames(counts)) %>% invisible
 counts[, sample_cell := paste(sample, "-", cell)]
 setkey(counts, sample_cell, chrom)
 
 
 
-### Read segment file
-seg = fread(f_segments)
-assert_that("chrom"      %in% colnames(seg),
-            "breakpoint" %in% colnames(seg),
-            "start"      %in% colnames(seg),
-            "end"        %in% colnames(seg))
-
 ### Read SV call table
+message(" * Reading SV calls from ", f_sv_calls, "...")
 svs = fread(f_sv_calls)
-assert_that("chrom"  %in% colnames(svs),
-            "start"  %in% colnames(svs),
-            "end"    %in% colnames(svs),
-            "sample" %in% colnames(svs),
-            "cell"   %in% colnames(svs),
-            "sv"     %in% colnames(svs))
+assert_that("chrom"    %in% colnames(svs),
+            "start"    %in% colnames(svs),
+            "end"      %in% colnames(svs),
+            "sample"   %in% colnames(svs),
+            "cell"     %in% colnames(svs),
+            "SV_class" %in% colnames(svs)) %>% invisible
+assert_that(all(svs$SV_class %in% names(manual_colors))) %>% invisible
+svs[, sample_cell := paste(sample, "-", cell)]
+assert_that(all(unique(svs$sample_cell) %in% unique(counts$sample_cell))) %>% invisible
 
 
 
-
-# @work!
-
-background_colors = data.table()
-manual_colors = c(ref = "#bbbbbb",
-                  del_hom = "dodgerblue4", del_h1 = "dodgerblue1", del_h2 = "dodgerblue3",
-                  inv_hom = "seagreen",    inv_h1 = "seagreen1",   inv_h2 = "seagreen3",
-                  dup_hom = "red4",        dup_h1 = "red1",        dup_h2 = "red3",
-                  idup_h1 = "yellow1",    idup_h2 = "yellow3",
-                  seg1 = "#edf8b1", seg2 = "#7fcdbb", seg3 = "#2c7fb8")
-
-
-
-
-    # Read 'other' table - only then we can find out which type of data it is
-    message(" * Reading addtional data ", f_other, "...")
-    if (grepl('\\.gz$',f_other)) {
-        other = fread(paste(zcat_command, f_other))
-    } else {
-        other = fread(f_other)
-    }
-    assert_that(is.data.table(other),
-                "chrom" %in% colnames(other),
-                "start" %in% colnames(other),
-                "end"   %in% colnames(other))
+### Read simulated variants
+message(" * Reading simulated variants from ", f_sv_simul, "...")
+simul = fread(f_sv_simul)
+assert_that("chrom"   %in% colnames(simul),
+            "start"   %in% colnames(simul),
+            "end"     %in% colnames(simul),
+            "sample"  %in% colnames(simul),
+            "cell"    %in% colnames(simul),
+            "SV_type" %in% colnames(simul)) %>% invisible
+simul[, `:=`(SV_class = paste0("simul_",SV_type), SV_type = NULL, sample_cell = paste(sample, "-", cell))]
 
 
 
-    ##################
-    # Switch data type
-    #
-    ### SV prob file
-    if (all(c("sample","cell","p_ref") %in% colnames(other))) {
-
-            if (length(splitted)>1) {
-                message("You specified an SV prob file, but additional '='. This confuses me.")
-                stop()
-            }
-
-            message("   -> detected SV probability file")
-            prob = other
-
-            prob[, sample_cell := paste(sample, "-", cell)]
-
-            # Remove CN columns
-            if (length(grep("^p_cn", colnames(prob)))>0) {
-                prob[, grep("^p_cn", colnames(prob)) := NULL]
-            }
-
-            # Add prior probabilities: this is not the correct way to do it! [TODO]
-            prob$p_ref = prob$p_ref * 1.1
-
-            # Check that cells are the same as in "counts"
-            c1 = unique(counts[,.(sample_cell)])
-            c2 = unique(prob[,.(sample_cell)])
-            c3 = c1[c2,, on = .(sample_cell), nomatch=0]
-
-            if(nrow(c1) != nrow(c3)) {
-                message("WARNING: Not all the cells from the count table are also covered in the probabilities")
-                message("Cells in the count table:")
-                print(paste(c1$sample_cell))
-                message("Cells in the probability table:")
-                print(paste(c2$sample_cell))
-                message("Cells of the count table that are also covered by probs:")
-                print(paste(c3$sample_cell))
-            }
-
-            #######################
-            # Reformat prob as long
-            background_colors <- melt(prob,
-                              id.vars = c("chrom","start","end","sample_cell"),
-                              measure.vars = colnames(prob)[grepl('^p_',colnames(prob))],
-                              variable.name = "SV_class",
-                              value.name    = "probability")
-
-            background_colors[, SV_class := substr(SV_class,3,nchar(as.character(SV_class)))]
-
-
-            # Select
-            background_colors <- background_colors[, .SD[probability == max(probability),][order(probability),][1,], by = .(chrom,start,end,sample_cell)]
-            setkey(background_colors, chrom, sample_cell)
-
-
-
-    ### Seg file
-    } else if (all(c("k","breakpoint") %in% colnames(other))) {
-
-
-            message("   -> detected segmentation file")
-            segments = other[, .SD[k == quantile(1:max(k), f_seg_quantile, type = 3)], by = chrom]
-            assert_that(all(segments[, .N == k[1], by = chrom]$V1))
-            segments[, SV_class := rep(paste0("seg",1:3), ceiling(.N/3))[1:.N], by = chrom]
-
-
-            background_colors <- NULL
-            for ( samcell in unique(counts[,sample_cell]))
-                background_colors <- rbind(background_colors, cbind(segments, sample_cell = samcell))
-
-
-    ### other, unknown input
-    } else {
-            message("I don't understand the file ", f_other)
-            message("Segmentation files need to contain the columns k, breakpoint, chorm, start, end.")
-            message("SV prob files need to have columns chrom, start, end, sample, cell and then columns")
-            message("for the different SV probabilities, called p_ref, p_inv_het, p_del_hom, ...")
-            print_usage()
-            stop()
-    }
-} # !is.null(f_other)
-
+### Read segment file
+message(" * Reading segmentation file from ", f_segments, "...")
+seg = fread(f_segments)
+assert_that("chrom" %in% colnames(seg),
+            "bps"   %in% colnames(seg)) %>% invisible
+bins = unique(counts[, .(chrom, start, end)])
+seg = merge(seg, bins[,.N, by =chrom][, .(chrom, N = c(0,cumsum(N)[1:(.N-1)]))], by = "chrom")
+seg[, `:=`(from = c(1,bps[1:(.N-1)]+1), to = bps), by = chrom]
+seg[, `:=`(start = bins[from + N]$start,
+           end   = bins[to   + N]$end  )]
+seg[, SV_class := rep(c("bg1","bg2"),.N)[1:.N], by = chrom]
 
 
 
@@ -190,35 +139,44 @@ for (CHROM in unique(counts[, chrom])) {
 
     out = paste0(f_out,".",CHROM,".pdf")
     message(" * Plotting ", CHROM, " (", out, ")")
-    if (nrow(background_colors)>0 && "seg1" %in% background_colors$SV_class)
-        message("  -> Chose ", background_colors[chrom == CHROM, k][1], " segments (", f_seg_quantile, " quantile)")
-
 
     cairo_pdf(out, width=14, height=10, onefile = T)
 
+    # Plot only a couple of cells per page
     i = 1
     while (i <= n_cells) {
-
 
         # Subset to this set of cells:
         CELLS = unique(counts[,.(sample_cell)])[i:(min(i+cells_per_page-1,n_cells))]
         setkey(CELLS, sample_cell)
         local_counts = counts[CELLS, on = .(sample_cell), nomatch = 0][chrom == CHROM]
 
-        # Also subset the background coloring, if available at all
-        local_background_colors = data.table()
-        if (nrow(background_colors)>0) {
-            local_background_colors = background_colors[CELLS, on = .(sample_cell), nomatch=0][chrom == CHROM,]
-        }
+        local_svs = svs[CELLS, on = .(sample_cell), nomatch = 0][chrom == CHROM]
+        local_sim = simul[CELLS, on = .(sample_cell), nomatch = 0][chrom == CHROM]
 
+        # Segments need to be multiplied by "CELLS"
+        local_seg = seg[chrom == CHROM]
+        local_seg = CELLS[, cbind(local_seg, sample_cell), by = sample_cell]
+
+
+        # Start major plot
         plt <- ggplot(local_counts)
 
-        # Add GT colors:
-        if(nrow(local_background_colors)>0) {
+        # Add background colors:
+        if (nrow(local_seg)>0) {
             plt <- plt +
-                geom_rect(data = local_background_colors, alpha = 0.5, size = 0.1,
-                          aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf, fill = SV_class)) +
-                scale_fill_manual(values = manual_colors)
+                geom_rect(data = local_seg, alpha = 0.3,
+                          aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf, fill = SV_class)) 
+        }
+        if (nrow(local_svs)>0) {
+          plt <- plt +
+            geom_rect(data = local_svs, alpha = 1,
+                      aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf, fill = SV_class))
+        }
+        if (nrow(local_sim)>0) {
+          plt <- plt +
+            geom_rect(data = local_sim,
+                      aes(xmin = start, xmax = end, ymin = y_lim, ymax = Inf, fill = SV_class))
         }
 
         plt <- plt +
@@ -229,6 +187,7 @@ for (CHROM in unique(counts[, chrom])) {
             scale_x_continuous(breaks = pretty_breaks(12), labels = format_Mb) +
             scale_y_continuous(breaks = pretty_breaks(3)) +
             coord_cartesian(ylim = c(-y_lim, y_lim)) +
+            scale_fill_manual(values = manual_colors) +
             theme_minimal() +
             theme(panel.spacing = unit(0, "lines"),
                   axis.ticks.x = element_blank(),
