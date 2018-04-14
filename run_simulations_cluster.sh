54154a2cd8303eb505c80c8cee60a79b3d83cc3b
Sascha Meiers <meiers@embl.de>
2018-04-14 22:21:51 +0200

    Making this simulation ready for the cluster

--- run_simulations_cluster.sh
+++ run_simulations_cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+mkdir slurm 2> /dev/null
 snakemake -j 1000 \
-    --cluster-config cluster.json
+    --cluster-config cluster.json \
     --local-cores 1 \
     --cluster "{cluster.sbatch} -p {cluster.partition} --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
     --latency-wait 60 \
