b08724676cc1fb7d926ad5a1e12b615190c21758
Sascha Meiers <meiers@embl.de>
2018-04-17 11:20:31 +0200

    Changed number of cores to be used on the host machine (for local rules)

--- run_simulations_cluster.sh
+++ run_simulations_cluster.sh
@@ -3,7 +3,7 @@
 mkdir slurm 2> /dev/null
 snakemake -j 1000 \
     --cluster-config cluster.json \
-    --local-cores 1 \
+    --local-cores 8 \
     --cluster "{cluster.sbatch} -p {cluster.partition} --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
     --latency-wait 60 \
     --timestamp \
