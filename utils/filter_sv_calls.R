a94c253d15caac9eb56183a7025f2acf9ffbc67c
Sascha Meiers <meiers@embl.de>
2018-04-20 09:27:23 +0200

    Fixed typo

--- utils/filter_sv_calls.R
+++ utils/filter_sv_calls.R
@@ -1,7 +1,7 @@
 library(data.table)
 d = fread(snakemake@input[[1]])
 minsize = as.integer(snakemake@params[["minsvsize"]])
-mincells = as.integer(snakeamke@params[["minvaf"]]) / 100 * as.integer(snakemake@params[["numcells"]])
+mincells = as.integer(snakemake@params[["minvaf"]]) / 100 * as.integer(snakemake@params[["numcells"]])
 
 d = merge(d, d[, .N, by = .(chrom, start, end)], by = c("chrom", "start", "end"))
 write.table(d[end-start >= minsize & N >= mincells],
