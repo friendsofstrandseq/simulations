420f4df1870e006dd3f13f0156ec8f89a3570f2b
Sascha Meiers <meiers@embl.de>
2018-06-25 16:52:31 +0200

    better cluster support for (using cluster_status.py)

--- run_simulations_cluster.sh
+++ run_simulations_cluster.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 mkdir slurm 2> /dev/null
-snakemake -j 1000 \
+snakemake -j 2000 \
     --cluster-config cluster.json \
     --local-cores 8 \
-    --cluster "{cluster.sbatch} -p {cluster.partition} --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
+    --cluster "{cluster.sbatch} --parsable -p {cluster.partition} --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
     --latency-wait 60 \
     --timestamp \
     --keep-going \
-    --restart-times 1
+    --restart-times 1 \
+    --cluster-status ./cluster_status.py
