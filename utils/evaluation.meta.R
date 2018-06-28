fb5bd10292b62a8dfb9d59d14ad01fa41c704e6d
Sascha Meiers <meiers@embl.de>
2018-06-28 14:52:55 +0200

    Updated meta evaluation --> special plots to compare several SV calling methods next to each other

--- utils/evaluation.meta.R
+++ utils/evaluation.meta.R
@@ -20,42 +20,20 @@ for (f in snakemake@input) {
 }
 D
 
-cairo_pdf(snakemake@output[[1]], width = 16, height = 12, onefile = T)
-for (sv_class_ in unique(D$SIMUL_svclass)) {
-  for (sv_size_ in unique(D$SIMUL_minsize)) {
-    for (vaf_ in unique(D$SIMUL_minvaf) ) {
-      E = D[SIMUL_minsize == sv_size_ & SIMUL_minvaf == vaf_]
-      size_string = paste(format_Mb(unique(E$SIMUL_minsize)), "-", format_Mb(unique(E$SIMUL_maxsize)))
-      vaf_string  = paste0(unique(E$SIMUL_minvaf),"-",unique(E$SIMUL_maxvaf), " %")
+cairo_pdf(snakemake@output[[1]], width = 12, height = 8, onefile = T)
+for (SV_class_ in unique(D$SV_class)) {
+  for (SV_size_ in unique(D$SV_size)) {
+    for (SV_vaf_ in unique(D$SV_vaf) ) {
+      E = D[SV_size == SV_size_ & SV_class == SV_class_ & SV_vaf == SV_vaf_]
       p <- ggplot(E) +
-        geom_path(aes(bp.recall, bp.precision, col = method)) +
-        geom_point(aes(bp.recall, bp.precision, col = method)) +
+        geom_path(aes(recall, precision, col = method)) +
+        geom_point(aes(recall, precision, col = method)) +
         theme_bw() + 
         theme(legend.position = "bottom") +
         coord_cartesian(ylim = c(0,1), xlim = c(0,1)) + 
-        ggtitle(paste0("SV size ", size_string, " with a VAF of ", vaf_string))
+        ggtitle(paste0(SV_class_, "s with a size of ", SV_size_, " and a clonal fraction of ", SV_vaf_))
       print(p)
     }
   }
-}
-dev.off()
-
-
-
-cairo_pdf(paste0(sub(".pdf$", "", snakemake@output[[1]]), 2, ".pdf"), width = 12, height = 8, onefile = T)
-for (method_ in unique(D$method)) {
-  E = copy(D[method == method_])
-  E[, simulation := paste0(SV_size, "; VAF = ", SV_vaf)]
-  p = ggplot(E) +
-    geom_path(aes(bp.recall, bp.precision, col = SV_vaf)) +
-    geom_point(aes(bp.recall, bp.precision, col = SV_vaf)) +
-    theme_bw() +
-    theme(legend.position = "bottom") +
-    coord_cartesian(ylim = c(0,1), xlim = c(0,1)) +
-    ggtitle(method_) +
-    geom_vline(xintercept = 0.8, color = "red", linetype = "dotted") +
-    geom_hline(yintercept = 0.8, color = "red", linetype = "dotted") +
-    facet_wrap(~SV_size)
-  print(p)
 }
 dev.off()
